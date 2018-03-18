c116ef430bf4f39a67324bf329be8dbacac68653
Rafael Fiume <rafael.fiume@yahoo.it>
2018-03-18 12:45:26 +0000

    A simple quicksort implementation

--- src/Grooking/Sort.hs
+++ src/Grooking/Sort.hs
@@ -1,6 +1,9 @@
 module Grooking.Sort (
-    selectionSort
+    selectionSort,
+    quicksort
 ) where
+
+import Data.List
 
 selectionSort :: (Ord a) => [a] -> [a]
 selectionSort [] = []
@@ -14,3 +17,12 @@ theSmallerOne :: (Ord a) => (a, [a]) -> a -> (a, [a])
 theSmallerOne (prev, xs) e
     | prev <= e = (prev, e:xs)
     | otherwise = (e, prev:xs)
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort [x] = [x]
+quicksort xs = quicksort lower ++ pivots ++ quicksort higher
+    where pivot  = xs !! (length xs `div` 2) -- Ineficient use of (!!) Use Array instead
+          lower  = filter (< pivot) xs
+          higher = filter (> pivot) xs
+          pivots = filter (== pivot) xs
--- test/Grooking/SortSpec.hs
+++ test/Grooking/SortSpec.hs
@@ -1,15 +1,23 @@
 module Grooking.SortSpec where
 
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec (Spec, describe, context, it, shouldBe)
 
 import Grooking.Sort
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
 spec_sort :: Spec
-spec_sort = describe "Sort" $
-        it "sorts a list (Selection Sort)" $ do
+spec_sort = describe "Sort" $ do
+    context "selectionSort" $
+        it "sorts a list" $ do
+            selectionSort [] `shouldBe` ([] :: [Int])
+            selectionSort "a" `shouldBe` "a"
             selectionSort [11,2,9,7,10,12,1] `shouldBe` [1,2,7,9,10,11,12]
             selectionSort "mlhyyrhd" `shouldBe` "dhhlmryy"
-            selectionSort "a" `shouldBe` "a"
-            selectionSort [] `shouldBe` ([] :: [Int])
+
+    context "quicksort" $
+        it "sorts a list" $ do
+            quicksort [] `shouldBe` ([] :: [Int])
+            quicksort "a" `shouldBe` "a"
+            quicksort [11,2,9,7,10,9,12,1] `shouldBe` [1,2,7,9,9,10,11,12]
+            quicksort "mlhyyrhd" `shouldBe` "dhhlmryy"
